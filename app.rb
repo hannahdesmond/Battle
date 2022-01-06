35a4887486c501698a7bd79a1573a9df19cc231c
Hannah Desmond <desmond.hannah@googlemail.com>
2022-01-06 17:56:31 +0000

    Add GET '/play' route

--- app.rb
+++ app.rb
@@ -15,6 +15,12 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_one_name] = params[:player_one_name]
     session[:player_two_name] = params[:player_two_name]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     erb(:play)
   end
 
