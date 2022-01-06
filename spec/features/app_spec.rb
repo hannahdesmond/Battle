7d0e14901293ab4b309c4e69be800446a8ba9d0a
Hannah Desmond <desmond.hannah@googlemail.com>
2022-01-06 16:13:32 +0000

    Add name submission feature test

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -5,3 +5,13 @@ feature 'feature' do
     end
 end
 
+feature 'name submission' do
+  scenario 'players fill in names, seen on screen' do
+    visit('/')
+    fill_in 'Player One Name', with: 'Joy'
+    fill_in 'Player Two Name', with: 'Hannah'
+    click_button('Submit')
+    expect(page).to have_content('Player 1: Joy, Player 2: Hannah')
+  end
+end
+
